db6cd7970b0f0f50fff928aab474dd70e31775ac
Mark Hedley Jones <markhedleyjones@gmail.com>
2025-11-05 17:56:09 +0900

    Update tests for Dockerfile linting

--- tests/test.sh
+++ tests/test.sh
@@ -4,9 +4,9 @@ set -u
 
 # Detect container runtime (Docker or Podman)
 detect_container_runtime() {
-	if command -v podman &> /dev/null; then
+	if command -v podman &>/dev/null; then
 		echo "podman"
-	elif command -v docker &> /dev/null; then
+	elif command -v docker &>/dev/null; then
 		echo "docker"
 	else
 		echo "Error: Neither Docker nor Podman found. Please install one of them." >&2
@@ -20,7 +20,7 @@ width_of_test_titles=40
 
 test_basedir="/tmp/docker-bbq"
 
-BASEDIR="$( cd "$( dirname "$(dirname "${BASH_SOURCE[0]}")")" >/dev/null 2>&1 && pwd )"
+BASEDIR="$(cd "$(dirname "$(dirname "${BASH_SOURCE[0]}")")" >/dev/null 2>&1 && pwd)"
 TESTDIR="${test_basedir}/temporary"
 TESTREPO="test"
 
@@ -31,13 +31,17 @@ rm -rf ${test_basedir}
 mkdir -p ${test_basedir}
 
 test_sequence=(
-  bbq-create.sh
-  building-repositories.sh
-  run-command.sh
+	bbq-create.sh
+	building-repositories.sh
+	run-command.sh
+)
+
+lint_sequence=(
+	dockerfile-linting.sh
 )
 
 usage() {
-  cat <<EOF
+	cat <<EOF
 Usage: $(basename "${BASH_SOURCE[0]}") [options]
 
   Run the standard test-suit on docker-bbq
@@ -46,14 +50,15 @@ OPTIONS:
 
   -h, --help       Print this help and exit
   -v, --verbose    Print debug information on test failure
-  -a, --all        Run standard and template repository tests
+  -a, --all        Run standard, template, and linting tests
+  -l, --lint       Run only Dockerfile linting tests
   -t, --templates  Run only the template repository tests
   -d, --debug      Abort on first failure and don't clean-up
 
   --template <template-name>
                    Run only the tests against the specified template name
 EOF
-  exit
+	exit
 }
 
 # Used to specify a specific template to test
@@ -61,19 +66,19 @@ test_template=NULL
 
 # Parse parameters
 while :; do
-  case "${1-}" in
-  -h | --help) usage ;;
-  -v | --verbose) verbosity=1 ;;
-  -t | --templates) test_sequence=(templates.sh) ;;
-  -t | --template) test_sequence=(templates.sh) && test_template=$2 && shift ;;
-  -d | --debug) abort_on_failure=1 ;;
-  -a | --all) test_sequence+=(templates.sh) ;;
-  -?*) error "Unknown option: $1" ;;
-  *) break ;;
-  esac
-  shift
+	case "${1-}" in
+	-h | --help) usage ;;
+	-v | --verbose) verbosity=1 ;;
+	-l | --lint) test_sequence=("${lint_sequence[@]}") ;;
+	-t | --templates) test_sequence=(templates.sh) ;;
+	-t | --template) test_sequence=(templates.sh) && test_template=$2 && shift ;;
+	-d | --debug) abort_on_failure=1 ;;
+	-a | --all) test_sequence+=(templates.sh "${lint_sequence[@]}") ;;
+	-?*) error "Unknown option: $1" ;;
+	*) break ;;
+	esac
+	shift
 done
-
 
 # The following variables are available to testfiles
 export TESTDIR
@@ -82,12 +87,12 @@ export DOCKER_BBQ_NON_INTERACTIVE=1
 
 # Override locations to remove need for installation before test
 bbq-create() {
-  echo "${BASEDIR}/bin/bbq-create $*"
-  "${BASEDIR}/bin/bbq-create" $*
+	echo "${BASEDIR}/bin/bbq-create $*"
+	"${BASEDIR}/bin/bbq-create" $*
 }
 
 run() {
- "${BASEDIR}/bin/run" $*
+	"${BASEDIR}/bin/run" $*
 }
 
 expected_return_code=NULL
@@ -99,100 +104,100 @@ heading_spacer=""
 subheading_spacer=""
 
 heading() {
-  printf "${heading_spacer}"
-  heading_spacer='\n'
-  echo $1
+	printf "${heading_spacer}"
+	heading_spacer='\n'
+	echo $1
 }
 
 subheading() {
-  printf "${subheading_spacer}"
-  subheading_spacer='\n'
-  echo " * ${1^^}:"
+	printf "${subheading_spacer}"
+	subheading_spacer='\n'
+	echo " * ${1^^}:"
 }
 
 run_test() {
-  if [ "${name}" == NULL ]; then
-    exit "Test does not have a name!"
-    exit 1
-  fi
-  printf " - "
-  printf "%-${width_of_test_titles}s" "${name}"
-  printf " ... "
-  eval "$*" > "${TESTDIR}/output" 2>&1
-  local return_code=$?
-  if [ "${return_code}" == "${expected_return_code}" ] ; then
-    echo -e "\e[32mSUCCESS\e[0m"
-    ((passed_tests_subtotal=passed_tests_subtotal+1))
-    local success=1
-  else
-    echo -e "\e[31mFAILED!\e[0m"
-    local success=0
-  fi
-  ((num_tests_subtotal=num_tests_subtotal+1))
-  name=NULL
-  output=$(cat "${TESTDIR}/output")
-  if [ ${success} -eq 0 ] && [ ${verbosity} -eq 1 ]; then
-    if [ -s "${TESTDIR}/output" ]; then
+	if [ "${name}" == NULL ]; then
+		exit "Test does not have a name!"
+		exit 1
+	fi
+	printf " - "
+	printf "%-${width_of_test_titles}s" "${name}"
+	printf " ... "
+	eval "$*" >"${TESTDIR}/output" 2>&1
+	local return_code=$?
+	if [ "${return_code}" == "${expected_return_code}" ]; then
+		echo -e "\e[32mSUCCESS\e[0m"
+		((passed_tests_subtotal = passed_tests_subtotal + 1))
+		local success=1
+	else
+		echo -e "\e[31mFAILED!\e[0m"
+		local success=0
+	fi
+	((num_tests_subtotal = num_tests_subtotal + 1))
+	name=NULL
+	output=$(cat "${TESTDIR}/output")
+	if [ ${success} -eq 0 ] && [ ${verbosity} -eq 1 ]; then
+		if [ -s "${TESTDIR}/output" ]; then
 
-      printf "\n######### DEBUG OUTPUT FROM FAILED TEST START #########\n"
-      cat "${TESTDIR}/output"
-      printf "########## DEBUG OUTPUT FROM FAILED TEST END ##########\n\n"
-    else
-      echo "   FAILED TEST PRODUCED NO DEBUG OUTPUT"
-    fi
-    echo "Test command: $*"
-  fi
-  if [[ ${success} -eq 0 ]] && [[ ${abort_on_failure} -eq 1 ]]; then
-    echo "Aborting on failure due to --debug flag"
-    echo "State on failure can be found at ${TESTDIR}"
-    exit 1
-  fi
-  return ${success}
+			printf "\n######### DEBUG OUTPUT FROM FAILED TEST START #########\n"
+			cat "${TESTDIR}/output"
+			printf "########## DEBUG OUTPUT FROM FAILED TEST END ##########\n\n"
+		else
+			echo "   FAILED TEST PRODUCED NO DEBUG OUTPUT"
+		fi
+		echo "Test command: $*"
+	fi
+	if [[ ${success} -eq 0 ]] && [[ ${abort_on_failure} -eq 1 ]]; then
+		echo "Aborting on failure due to --debug flag"
+		echo "State on failure can be found at ${TESTDIR}"
+		exit 1
+	fi
+	return ${success}
 }
 
 pass() {
-  expected_return_code=0
-  run_test $*
-  return $?
+	expected_return_code=0
+	run_test $*
+	return $?
 }
 
 fail() {
-  expected_return_code=1
-  run_test $*
-  return $?
+	expected_return_code=1
+	run_test $*
+	return $?
 }
 
 clean() {
-  # Reset output/formatting variables
-  subheading_spacer=''
-  num_tests_subtotal=0
-  passed_tests_subtotal=0
+	# Reset output/formatting variables
+	subheading_spacer=''
+	num_tests_subtotal=0
+	passed_tests_subtotal=0
 
-  # Regenerate clean test directories
-  rm -rf ${TESTDIR}
-  mkdir -p ${TESTDIR}
-  cd ${TESTDIR}
+	# Regenerate clean test directories
+	rm -rf ${TESTDIR}
+	mkdir -p ${TESTDIR}
+	cd ${TESTDIR}
 
-  # Kill any already running containers with the TESTREPO's name
-  running="$(${CONTAINER_RUNTIME} ps --quiet --filter name="${TESTREPO}")"
-  if [ ! -z "${running}" ]; then
-    ${CONTAINER_RUNTIME} kill "${running}"
-  fi
+	# Kill any already running containers with the TESTREPO's name
+	running="$(${CONTAINER_RUNTIME} ps --quiet --filter name="${TESTREPO}")"
+	if [ ! -z "${running}" ]; then
+		${CONTAINER_RUNTIME} kill "${running}"
+	fi
 }
 
 for filename in ${test_sequence[*]}; do
-  clean
-  heading "Running ${filename%.sh} tests ... "
-  source "${BASEDIR}/tests/definitions/${filename}"
-  echo "   Completed: ${passed_tests_subtotal} / ${num_tests_subtotal} test(s) passed"
-  ((num_tests_total=num_tests_total+num_tests_subtotal))
-  ((passed_tests_total=passed_tests_total+passed_tests_subtotal))
+	clean
+	heading "Running ${filename%.sh} tests ... "
+	source "${BASEDIR}/tests/definitions/${filename}"
+	echo "   Completed: ${passed_tests_subtotal} / ${num_tests_subtotal} test(s) passed"
+	((num_tests_total = num_tests_total + num_tests_subtotal))
+	((passed_tests_total = passed_tests_total + passed_tests_subtotal))
 done
 
 echo ""
 echo "Testing completed - ${passed_tests_total} / ${num_tests_total} test(s) passed"
-if [ ${passed_tests_total} -eq ${num_tests_total} ] ; then
-  exit 0
+if [ ${passed_tests_total} -eq ${num_tests_total} ]; then
+	exit 0
 else
-  exit 1
+	exit 1
 fi
