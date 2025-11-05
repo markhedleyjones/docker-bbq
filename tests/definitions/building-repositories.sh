31dcbf31836b7195ce5ed5b0e13e16a8eef412b6
Mark Hedley Jones <markhedleyjones@gmail.com>
2025-11-06 06:49:32 +0900

    building-repositories.sh: Remove slashes from hyphens

--- tests/definitions/building-repositories.sh
+++ tests/definitions/building-repositories.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-bbq-create debian ${TESTREPO} > /dev/null 2>&1
+bbq-create debian ${TESTREPO} >/dev/null 2>&1
 cd ${TESTREPO}
 
 name="Building development image (debian)"
 pass make
 
 name="Privileges enabled on devlopment image"
-is_privileged=$(TESTING=1 run | tail -n 1 | grep -c " \-\-privileged ")
+is_privileged=$(TESTING=1 run | tail -n 1 | grep -c " --privileged ")
 pass test ${is_privileged} -eq 1
 
 name="Building production image (debian)"
 pass make production
 
 name="Privileges disabled on production image"
-is_privileged=$(TESTING=1 run | tail -n 1 | grep -c " \-\-privileged ")
+is_privileged=$(TESTING=1 run | tail -n 1 | grep -c " --privileged ")
 pass test ${is_privileged} -eq 0
